5d2ffed43e06c30737ff2df9b81425e19c4b7096
1k <1k@iMac-de-iMac13.local>
2018-11-30 19:38:21 -0600

    3

--- peliculas/js/main.js
+++ peliculas/js/main.js
@@ -1,4 +1,18 @@
 import Pelicula from "./pelicula.js";
+
+var titulo = document.querySelector('·titulo');
+var año = document.querySelector('#año');
+var duracion = document.querySelector('#duracion');
+
+document.querySelector('#btnAgregar')
+.addEventListener('click', onAgregar);
+
+function onAgregar () {
+    let m1 = new Pelicula(titulo.value, Number(año.value), Number(duracion.value));
+    m1.imprimir();
+}
+
+
 var m1 = new Pelicula('Scary Movie', 2000, 126);
 var m2 = new Pelicula('Toy Story', 2014, 200);
 m1.imprimir();
