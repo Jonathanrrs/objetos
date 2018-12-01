197f9f44ee634a004afc44e8593180a8e6df6856
1k <1k@iMac-de-iMac13.local>
2018-11-30 19:12:39 -0600

    2

--- peliculas/js/main.js
+++ peliculas/js/main.js
@@ -1,9 +1,5 @@
-class Pelicula {
-    constructor(titulo) {
-        this.nombrePelicula = titulo;
-    }
-    imprimir() {
-        console.log(this.nombrePelicula);
-
-    }
-}
+import Pelicula from "./pelicula.js";
+var m1 = new Pelicula('Scary Movie', 2000, 126);
+var m2 = new Pelicula('Toy Story', 2014, 200);
+m1.imprimir();
+m2.imprimir();
